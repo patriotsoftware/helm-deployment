1d42ef2424ddb0c91f718d8ec1df21f046c6ece6
noneill <noneill@patriotsoftware.com>
2024-11-11 13:12:00 -0500

    Debugging

--- helm-chart-pull-action.sh
+++ helm-chart-pull-action.sh
@@ -2,6 +2,8 @@
 export HELM_EXPERIMENTAL_OCI=1
 set -e
 
+mkdir -p $TARGET_DIRECTORY
+
 helm pull "oci://${BASE_CHART}" --untar --untardir "${TARGET_DIRECTORY}" $CHART_VERSION
 echo "✅ ${BASE_CHART} pulled successfully"
 echo "✅ ${BASE_CHART} saved to ${TARGET_DIRECTORY} successfully"
