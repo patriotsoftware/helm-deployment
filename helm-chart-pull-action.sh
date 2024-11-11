57ea23ec4998f0032b224c08ab01259daab2e8c3
noneill <noneill@patriotsoftware.com>
2024-11-11 13:28:44 -0500

    Debugging

--- helm-chart-pull-action.sh
+++ helm-chart-pull-action.sh
@@ -4,8 +4,14 @@ set -e
 
 mkdir -p $TARGET_DIRECTORY
 
+if [[ "${ls $TARGET_DIRECTORY}" != 1 ]];
+    echo "The target directory was not empty: $TARGET_DIRECTORY"
+    exit 1
+fi
+
 helm pull "oci://${BASE_CHART}" --untar --untardir "${TARGET_DIRECTORY}" $CHART_VERSION
 echo "✅ ${BASE_CHART} pulled successfully"
 echo "✅ ${BASE_CHART} saved to ${TARGET_DIRECTORY} successfully"
+
 CHART_PATH="$TARGET_DIRECTORY$(ls $TARGET_DIRECTORY)"
 echo "chart-path=$CHART_PATH" >> "$GITHUB_OUTPUT"
